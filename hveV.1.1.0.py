5033d5f6073f091c048938f3101f580419eaf52d
Holo the not so wise wolf <holothenotsowise@gmail.com>
2017-04-28 14:55:15 -0400

    Update hveV.1.1.0.py

--- hveV.1.1.0.py
+++ hveV.1.1.0.py
@@ -3,17 +3,16 @@ import ssl
 import httplib 
 import requests 
 
-proxies = {}
+proxies = {'https': 'socks5://user:pass@host:port'} #identify with actual proxy 
 
-RHOST = 'https://commandandconquer.net'
+RHOST = 'https://commandandconquer.net' #replace with a C&C server
 PORT = 80
-
-resp = requests.get(RHOST, proxies)
+#todo add packet information. Maybe craft packets to use against windows and linux. Need a scanner added to enviroment to test for OS
+resp = requests.get(RHOST, proxies) #setting RHOST and the proxy connection 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.settimeout(10)
-wrappedSocket = ssl.wrap_socket(sock, ssl_version=ssl.PROTOCOL_TLSv1, ciphers="TLS_RSA_WITH_RC4_128_SHA")
+wrappedSocket = ssl.wrap_socket(sock, ssl_version=ssl.PROTOCOL_TLSv1, ciphers="TLS_RSA_WITH_RC4_128_SHA") #generic RSA with 128 SHA for SSL 
 wrappedSocket.connect((HOST, PORT))  #set HOST and port to agree with resp
-wrappedSocket.send(packet)
-print wrappedSocket.recv(1280)
-wrappedSocket.close()
-
+wrappedSocket.send(packet) #this is where packet is declared but no packet currently exists 
+print wrappedSocket.recv(1280) #outprint recieved 
+wrappedSocket.close() #close wrapper
